d4e8ca3c4959f562f9dcc00aa7286e607f7a6057
ShintaroSugawara <sugawgra.shintaro@gmail.com>
2026-09-02 11:00:52 +0900

    FastAPI-Trial-2

--- api/db.py
+++ api/db.py
@@ -1,8 +1,25 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8mb4"
+DEFAULT_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8mb4"
+
+ASYNC_DB_URL = os.getenv(
+    "MYSQL_URL",
+    DEFAULT_DB_URL
+)
+
+# RailwayのMYSQL_URLは mysql:// なので
+# SQLAlchemyの非同期MySQL用URLに変換する
+if ASYNC_DB_URL.startswith("mysql://"):
+    ASYNC_DB_URL = ASYNC_DB_URL.replace(
+        "mysql://",
+        "mysql+aiomysql://",
+        1
+    )
+
 
 async_engine = create_async_engine(
     ASYNC_DB_URL,
@@ -22,4 +39,3 @@ Base = declarative_base()
 async def get_db():
     async with async_session() as session:
         yield session
-
